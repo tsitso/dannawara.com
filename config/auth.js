4f8ea2f3dc0ffc2f0a70f138a9c0d1eb4ac63e8f
Dan Nawara <dan@pinmonkey.com>
2013-02-22 19:30:58 -0600

    moving domain to nodejitsu

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   github: {
-    appId: '9cf6b585445bcc1c7e7f'
-  , appSecret: 'a687f62836b533d3b7f02958c4136050e0293625'
+    appId: '1a1eaf50f47533d57c8d'
+  , appSecret: '718c0b8ed76484f88c365bb85f5a9ee3b7e286b6'
   }
 , google: {
     appId: '742520875094.apps.googleusercontent.com'
